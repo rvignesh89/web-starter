557db8267ec9138fd50f4355a6a542be5d011c62
vignesh <rvignesh@outlook.com>
2015-10-21 12:46:16 +0800

    add test project

---
+++ web-starter/web-starter.tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace web_starter.tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ web-starter/web-starter/AppStarter.cs
@@ -0,0 +1,6 @@
+﻿namespace web_starter
+{
+    public class AppStarter
+    {
+    }
+}
